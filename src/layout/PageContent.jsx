8da3532eeb8ac429a59ea6747dea5dc85906ebd8
bernakocalar <berna.kocalar@gmail.com>
2025-04-09 23:25:04 +0300

    fix: correct import case for HomePage in PageContent component

--- src/layout/PageContent.jsx
+++ src/layout/PageContent.jsx
@@ -1,6 +1,6 @@
 import Header from "./Header";
 import MainNav from "./MainNav";
-import HomePage from "../pages/homePage";
+import HomePage from "../pages/HomePage";
 
 export default function PageContent() {
     return (
