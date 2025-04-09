46a22b06a7a66e64cbd79cdf5d1f84abcbdd0546
bernakocalar <berna.kocalar@gmail.com>
2025-04-09 23:23:31 +0300

    fix: correct import case for PageContent in App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 // App.jsx veya index.js içinde
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import PageContent from "./layout/pageContent";
+import PageContent from "./layout/PageContent";
 
 function App() {
   return <PageContent />
